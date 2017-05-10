31309d456418c972569d017d516fa9880b4e0f9e
Paul Chin <pchinjr@gmail.com>
2017-05-10 11:21:57 -0400

    added servo

--- proximity.js
+++ proximity.js
@@ -4,17 +4,27 @@ var board = new five.Board();
 board.on("ready", function() {
   var proximity = new five.Proximity({
     controller: "HCSR04",
-    pin: 7
+    pin: 7    
   });
 
+  var servo = new five.Servo.Continuous(10);
+
   proximity.on("data", function() {
+    if (this.in <= 3){
+        servo.stop();
+    } else {
+        servo.cw();
+    }
     console.log("Proximity: ");
     console.log("  cm  : ", this.cm);
     console.log("  in  : ", this.in);
     console.log("-----------------");
+    
   });
 
   proximity.on("change", function() {
     console.log("The obstruction has moved.");
   });
+
+  
 });
