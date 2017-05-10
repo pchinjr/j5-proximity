13f69b9ab89c17f4ea3fb01ced296f61a6bb1de4
Paul Chin <pchinjr@gmail.com>
2017-05-10 14:24:12 -0400

    working example with range control of servo

--- proximity.js
+++ proximity.js
@@ -4,27 +4,31 @@ var board = new five.Board();
 board.on("ready", function() {
   var proximity = new five.Proximity({
     controller: "HCSR04",
-    pin: 7    
+    pin: 7, 
+    freq: 300 
   });
 
   var servo = new five.Servo.Continuous(10);
 
   proximity.on("data", function() {
+    
     if (this.in <= 3){
-        servo.stop();
-    } else {
         servo.cw();
+    } else if(this.in > 3 && this.in <= 6){
+        servo.stop();
+    } else if(this.in > 6 ) {
+        servo.ccw();
     }
+
     console.log("Proximity: ");
     console.log("  cm  : ", this.cm);
     console.log("  in  : ", this.in);
     console.log("-----------------");
-    
   });
 
   proximity.on("change", function() {
     console.log("The obstruction has moved.");
   });
-
   
 });
+
